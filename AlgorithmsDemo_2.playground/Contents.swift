e677f0c5f7adca18ae2b6f615c2d06c03fb989ca
Subhra <subhra13heritage@gmail.com>
2019-06-11 00:49:44 +0530

    Reverse Link List

--- AlgorithmsDemo_2.playground/Contents.swift
+++ AlgorithmsDemo_2.playground/Contents.swift
@@ -2,7 +2,7 @@
 import Foundation
 
 
-let array1 = [1,2,3]
+/*let array1 = [1,2,3]
 let array2 = [4,2,2]
 
 extension Array where Element: Hashable {
@@ -18,4 +18,53 @@ array1.difference(from: array2)
 
 let array3 = [1,2,3,4,5]
 
-let sum = array3.reduce(0, +)
+let sum = array3.reduce(0, +)*/
+
+//*********Single Link List*******//
+//1->2->3->nil
+
+class Node{
+    var value : Int = 0
+    var nextNode : Node?
+    
+    init(value : Int, next : Node?) {
+        self.value = value
+        self.nextNode = next
+    }
+}
+
+
+let nodeThree : Node = Node(value: 3, next: nil)
+let nodeTwo : Node = Node(value: 2, next: nodeThree)
+let nodeOne : Node = Node(value: 1, next: nodeTwo)
+
+
+func printLinkListWith(node : Node?){
+    print("Link List :")
+    var currentNode = node
+    while currentNode != nil {
+        print("\(String(describing: currentNode?.value))")
+        currentNode = currentNode?.nextNode
+    }
+}
+
+printLinkListWith(node: nodeOne)
+//******Reverse Single Link List********//
+//nil<-1<-2<-3
+func reverseLiskListWith(head : Node?) -> Node?{
+    var currentNode = head
+    var next : Node?
+    var previous : Node?
+    while currentNode != nil {
+        next = currentNode?.nextNode
+        currentNode?.nextNode = previous
+        print("Previous : \(previous?.value ?? -1), Current : \(currentNode?.value ?? -1), Next : \(next?.value ?? -1)")
+        previous = currentNode
+        currentNode =  next
+    }
+    
+    return previous
+}
+
+let reverseLink = reverseLiskListWith(head: nodeOne)
+printLinkListWith(node: reverseLink)
