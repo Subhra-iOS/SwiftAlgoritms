6c8482c3adeb09d8339d6dad4d10d7ced70e52b7
Subhra <subhra13heritage@gmail.com>
2019-06-15 22:40:08 +0530

    Flatten Array

--- AlgorithmsDemo_3.playground/Contents.swift
+++ AlgorithmsDemo_3.playground/Contents.swift
@@ -92,3 +92,159 @@ func reverseEachWordIn(line : String) -> String{
 }
 
 print("\(reverseEachWordIn(line: sentence))")
+//********Fibonacci Sequence*******//
+let maxElement = 5 //[0, 1, 1, 2, 3 ]
+
+func createFibonacciSequenceWith(maxNo : Int) -> [Int]{
+    
+    let firstElement = 0
+    let secondElement = 1
+    
+    var array : [Int] = [Int]()
+    array.append(firstElement)
+    array.append(secondElement)
+    
+    for i in 2 ..< maxNo{
+        array.append(array[i - 1] + array[i - 2])
+    }
+    
+    return array
+}
+
+print("\(createFibonacciSequenceWith(maxNo: maxElement))")
+//******** Wrapping an Array**********//
+let trackArray = ["a", "b", "c", "d", "e"]
+func wrapingAnArrayWith(trackArr : [String], selectedTrack : String) -> [String]{
+    var playTrackArr : [String] = [String]()
+    var priorTrackArr : [String] = [String]()
+
+    for track in trackArr{
+        
+        if track == selectedTrack || playTrackArr.count > 0{
+            playTrackArr.append(track)
+        }else{
+            priorTrackArr.append(track)
+        }
+    }
+
+    return playTrackArr + priorTrackArr
+    
+}
+
+let wapperArray = wrapingAnArrayWith(trackArr: trackArray, selectedTrack: "d")
+print("\(wapperArray)")
+
+let prefixArray = wapperArray.prefix(upTo: 3)
+let suffixArray = wapperArray.suffix(from: 3)
+//**********Reduce Array**********//
+let numbersArr = [1, 2, 3, 4]
+let reduceArray = numbersArr.reduce(0, {sum, number in sum + number})
+print("\(reduceArray)")
+//************Recursive Binary search tree********//
+/*     10
+        / \
+    5     14
+   /        /  \
+1         11   20
+*/
+
+class Node{
+    var value : Int = 0
+    var leftNode : Node?
+    var rightNode : Node?
+    
+    init(value : Int, leftChild : Node?, rightChild : Node?) {
+        self.value = value
+        self.leftNode = leftChild
+        self.rightNode = rightChild
+    }
+}
+let twentyNode = Node(value: 20, leftChild: nil, rightChild: nil)
+let elevenNode = Node(value: 11, leftChild: nil, rightChild: nil)
+let oneNode = Node(value: 1, leftChild: nil, rightChild: nil)
+
+let forteenNode = Node(value: 14, leftChild: elevenNode, rightChild: twentyNode)
+let fiveNode = Node(value: 5, leftChild: oneNode, rightChild: nil)
+let rootNode = Node(value: 10, leftChild: fiveNode, rightChild: forteenNode)
+
+func searchInTreeWith(node : Node?, searchValue : Int) -> Bool{
+    guard let treeNode : Node = node else { return false }
+    
+    if treeNode.value == searchValue{ return true }
+    else if searchValue < treeNode.value {
+       return searchInTreeWith(node: treeNode.leftNode, searchValue: searchValue)
+    }else {
+        return searchInTreeWith(node: treeNode.rightNode, searchValue: searchValue)
+    }
+}
+
+print("\(searchInTreeWith(node: rootNode, searchValue: 20))")
+
+// 5 * 25 + 6
+//            +
+//        /     \
+//      *       5
+//     /  \
+//    25 6
+
+
+class OperationNode{
+    var operation : String? // +, - , *, /
+    var value : Int?
+    var leftNode : OperationNode?
+    var rightNode : OperationNode?
+    init(operation : String?, value : Int?, left : OperationNode?, right : OperationNode?) {
+        self.operation = operation
+        self.value = value
+        self.leftNode = left
+        self.rightNode = right
+    }
+}
+
+let node_25 = OperationNode(operation: nil, value: 25, left: nil, right: nil)
+let node_6 = OperationNode(operation: nil, value: 6, left: nil, right: nil)
+let node_multiply_25_6 = OperationNode(operation: "*", value: nil, left: node_25, right: node_6)
+let node_5 = OperationNode(operation: nil, value: 5, left: nil, right: nil)
+let rootPlusNode = OperationNode(operation: "+", value: nil, left: node_multiply_25_6, right: node_5)
+
+func evaluateNode(node : OperationNode?) -> Int{
+    
+    guard let currentNode : OperationNode = node else { return 0 }
+    if currentNode.value != nil{
+        return currentNode.value!
+    }else if currentNode.operation == "+" {
+        return  evaluateNode(node: currentNode.leftNode) + evaluateNode(node: currentNode.rightNode)
+    }else if currentNode.operation == "*" {
+        return  evaluateNode(node: currentNode.leftNode) * evaluateNode(node: currentNode.rightNode)
+    }else if currentNode.operation == "-" {
+        return  evaluateNode(node: currentNode.leftNode) * evaluateNode(node: currentNode.rightNode)
+    }else if currentNode.operation == "/",  let rightChild = currentNode.rightNode, let rightValue = rightChild.value, rightValue > 0 {
+        return  evaluateNode(node: currentNode.leftNode) * evaluateNode(node: currentNode.rightNode)
+    }
+    
+    return 0
+}
+
+print("\(evaluateNode(node: rootPlusNode))")
+
+//********Flatten a nested Array*************//
+//[1, [2, [3, 4] ] ] ----> [1, 2, 3, 4]
+
+let nonFlattenArray : [Any] = [ [-1, 0],1, [2, [3, [4, 5, [6, 7, 8] ] ] ] ]
+func flattenArrayWith(array : [Any]) -> [Int]{
+    var myFlattenArr : [Int] = [Int]()
+    for ele in array{
+        if ele is Int {
+            myFlattenArr.append(ele as! Int)
+        }else if ele is [Any]{
+            let result = flattenArrayWith(array: ele as! [Any])
+            for element in result {
+                myFlattenArr.append(element)
+            }
+        }
+        
+    }
+    return myFlattenArr
+}
+
+print("\(flattenArrayWith(array: nonFlattenArray))")
